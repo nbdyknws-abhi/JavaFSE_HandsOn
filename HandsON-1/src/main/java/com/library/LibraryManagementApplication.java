d9e60ff09250be8ebbb3c97412d634798af2f75e
Abhishek Verma <av022002@gmail.com>
2025-07-04 18:23:32 +0530

    Add files via upload

---
+++ HandsON-1/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,18 @@
+package com.library;
+
+import com.library.service.BookService;
+import com.library.repository.BookRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service = (BookService) context.getBean("bookService");
+        BookRepository repo = (BookRepository) context.getBean("bookRepository");
+
+        service.display();
+        repo.show();
+    }
+}
